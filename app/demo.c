24a46da16825bd036ce0ec24aa92c63d1e4f19b4
Jshaktiraj <shaktijad@gmail.com>
2020-05-09 20:15:01 +0530

    C programm updated

--- app/demo.c
+++ app/demo.c
@@ -2,5 +2,12 @@
 #include <conio.h>
 void main()
 {
-	printf("Hello world");
+	int n1,n2,ans;
+	printf("Enter Number 1");
+	scanf("%d",&n1);
+	printf("Enter Number 1");	
+	scanf("%d",&n2);
+	ans = n1 + n2;
+	printf("Answer %d",ans);
+	getch();
 }
